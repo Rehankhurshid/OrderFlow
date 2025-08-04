6b6ba7bc679c508bebcdf351f66880eaece13876
Rehan Khurshid <rehankhurshid1@gmail.com>
2025-08-04 09:59:46 +0530

    Fix api/index.ts to use createApp from server/app.ts

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,9 @@
 export default async function handler(req: any, res: any) {
-  // Import the Express app
-  const { app } = await import('../server/index.js');
+  // Import the Express app creator
+  const { createApp } = await import('../server/app.js');
+  
+  // Create and get the Express app
+  const app = await createApp();
   
   // Let Express handle the request
   app(req, res);
